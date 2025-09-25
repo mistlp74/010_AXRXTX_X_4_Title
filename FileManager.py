00b3fe04dd0c4bbabbe70f9b92cef5940c0938a7
mistlp74 <dmytrobryzhatiuk@gmail.com>
2025-09-25 14:42:09 +0200

    Оновлення

--- FileManager.py
+++ FileManager.py
@@ -1,19 +1,31 @@
 import json
+import random
+from random import randint
 
-def load_users_from_json():
+
+def load_users():
     try:
         with open('data/users.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
+            print("users.json was loaded successfully")
             return data
-    except (FileNotFoundError, json.JSONDecodeError):
-        print("loading users.json failed")
-
+    except:
+        print("Error loading users.json")
         default_users = {
-            "User": {
-                "username": "User",
-                "password": "12345678",
+            "Username": {
+                "password": "12345678"
             }
         }
         with open('data/users.json', 'w', encoding='utf-8') as file:
             json.dump(default_users, file, indent=2, ensure_ascii=False)
         return default_users
+
+def create_user(username, password):
+    data = load_users()
+    data[username] = {"password": password}
+
+    with open('data/users.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+    with open('data/users.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-from FileManager import load_users_from_json
+from flask import Flask, render_template, request, redirect, url_for, render_template_string
+from FileManager import load_users, create_user
 import json
 
 app = Flask(__name__)
@@ -12,47 +12,41 @@ def home():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    users = load_users_from_json()
 
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
-        if not username or not password:
-            error = "Заповніть всі поля"
-            return render_template("signup.html", error=error, page="signup")
+        users = load_users()
 
         if username in users:
-            error = f"Користувач {username} вже існує!"
+            print("The user already exists")
+            error = "User already exists"
             return render_template("signup.html", error=error, page="signup")
 
-        users[username] = {
-            "username": username,
-            "password": password
-        }
+        create_user(username, password)
 
-        with open('users.json', 'w', encoding='utf-8') as file:
-            json.dump(users, file, indent=2, ensure_ascii=False)
-
-        success = f"Користувач {username} успішно створений!"
+        success = f"User {username} successfully created"
         return render_template("home.html", success=success, page="signup")
 
-    return render_template("signup.html", page="signup")
+    else:
+        return render_template("signup.html", page="signup")
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    users = load_users_from_json()
 
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
+        users = load_users()
+
         # Перевірка в users.json
         if username in users and users[username]['password'] == password:
             return redirect(url_for('home'))
         else:
-            error = 'Невірний логін або пароль'
+            error = 'Incorrect username or password'
             return render_template("login.html", error=error, page="login")
 
     return render_template("login.html", page="login")
